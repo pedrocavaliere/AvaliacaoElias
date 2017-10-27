c14b1e49c1286a410bd8329a180dc1d5e8ca8419
Aiko Ramalho <aiko269@hotmail.com>
2017-10-27 15:42:59 -0200

    Add UserAdapter, and recycler_user.xml

---
+++ app/src/main/java/com/example/pedrokuchpil/avaliacaoelias/UserAdapter.java
@@ -0,0 +1,10 @@
+package com.example.pedrokuchpil.avaliacaoelias;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by aiko.oliveira on 27/10/2017.
+ */
+
+public class UserAdapter{
+}
